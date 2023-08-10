2cab7ebb0c871a142b3dadf9f1ecc65c7e84307a
Javier <jmartin@grupoloyal.com>
2023-08-10 11:31:30 +0200

    MvcRequestMatcher

--- demo-mvc/src/main/java/com/example/AppSecurityConfig.java
+++ demo-mvc/src/main/java/com/example/AppSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
@@ -18,6 +19,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -42,12 +45,18 @@ public class AppSecurityConfig {
 		return new DefaultHttpFirewall();
 	}
 
+	@Scope("prototype")
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+		return new MvcRequestMatcher.Builder(introspector);
+	}
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
 		return http.cors(Customizer.withDefaults()).csrf((csrf) -> csrf.disable())
 				.authorizeHttpRequests(requests -> requests
 //						.requestMatchers("/*").permitAll()
-//						.requestMatchers("/ciudades/**").authenticated()
+						.requestMatchers(mvc.pattern("/ciudades/**")).authenticated()
 //						.requestMatchers("/actores/**").hasRole("MANAGER")
 //						.requestMatchers("/ajax/**").hasRole("ADMIN")
 						.anyRequest().permitAll())
