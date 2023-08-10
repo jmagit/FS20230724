b086ac730d1faca69e0fff2e2cda3a79df71b31a
Javier <jmartin@grupoloyal.com>
2023-08-10 11:58:13 +0200

    eliminar demo-web

--- demo-web/src/main/java/com/example/domains/entities/dtos/ElementoDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.domains.entities.dtos;
-
-import lombok.Value;
-
-@Value
-public class ElementoDto<K, V> {
-	K key;
-	V value;
-}
--- demo-web/src/main/webapp/WEB-INF/vistas/inicio.jsp
+++
@@ -1,3 +0,0 @@
-<%@ include file="parts/header.jsp"%>
-<h1>Hola mundo</h1>
-<%@ include file="parts/footer.jsp"%>
